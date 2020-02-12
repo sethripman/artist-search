6e200aa5b4c1141d86c00df0869aed34f1d69bca
Caleb Pendergraft <cackmed@gmail.com>
2020-02-12 11:09:52 -0800

    consolidated search view componets

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ListContainer from './ListContainer';
 import { useArtist } from '../hooks/artist.js';
+import { Link } from 'react-router-dom';
 
 const SearchView = () => {
-  const { artists,  handleSubmit, search, setSearch } = useArtist();
+  const { artists, search, setSearch } = useArtist();
+
+  const artistElements = artists.map((el, id) => {
+    return (
+      <li key={id}>
+        <Link>
+          <p>{el.name}</p>
+        </Link>
+      </li>
+    );
+  });
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={({ target }) => setSearch(target.value)} 
-          value={search} name="search"/>
-      </form>
-      <ListContainer listItems={artists}/>
+      <input type="text" onChange={({ target }) => setSearch(target.value)} 
+        value={search} name="search"/>
+
+      <ul>
+        {artistElements}
+      </ul>
     </>
   );
 };
